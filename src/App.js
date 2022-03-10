789c4cdc88dbb5f3cd3b168fe31159f96fa4fef0
PaLanzi <palanzillotta@googlemail.com>
2022-03-10 21:15:30 +0100

    getting startet with useeffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+//import { Routes, Route } from "react-router-dom";
+//import Form from "./Components/Form";
+//import Sidebar from "./Components/Sidebar";
+//import Post from "./Components/Post";
+//import Posts from "./Components/Posts";
 
 function App() {
+  const [messages, setMessages] = useState("");
+  const [searchValue, setSearchValue] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_YOBBQUQBY6trhgA9Up0JF0NkLEj1n`
+        );
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+          setMessages(data.ip, data.location.city);
+          console.log(data.ip, data.location.city);
+          setLoading(false);
+        } else {
+          console.error("....? nothing is happen");
+        }
+      } catch (e) {
+        console.log(e.message);
+        alert("Batman is coming!");
+      }
+    };
+    fetchMessages();
+  }, []);
+
+  function handleSearchValue(value) {
+    setSearchValue(value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img className="App-logo" alt="logo" />
+        <p>HELLO</p>
+        <p>{messages}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
